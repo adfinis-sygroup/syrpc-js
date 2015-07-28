a4dbf286d2aaf05e521bb8008eeda6713a76cf66
Jean-Louis Fuchs <ganwell@fangorn.ch>
2015-07-28 10:21:33 +0200

    Creating server

--- syrpc/base.js
+++ syrpc/base.js
@@ -2,10 +2,68 @@ var siphash = require("siphash")
 var consts  = require("./consts")
 
 export class SyRPCBase {
-
+  /**
+   * Creates the SyRPC class
+   *
+   * app_name        (mandatory) Every server this the same app name
+   *                             must support the same request-types
+   * amq_host        (mandatory)
+   * amq_virtualhost (optional)
+   * amq_user        (optional)
+   * amq_password    (optional)
+   * amq_transport   (optional) Not supported with js
+   * amq_ttl         (optional) Time to live for queues
+   * amq_msg_ttl     (optional) Time to live for messages
+   * amq_num_queues  (optional) Number of queue (default 64)
+   * msg_encoding    (optional) Default utf-8
+   *
+   * @constructor
+   * @param {settings} Settings object with fields  as above
+   */
   constructor(settings) {
-    this.key = siphash.string16_to_key(consts.AMQ_HASH)
-    this.num_queues = consts.AMQ_NUM_QUEUES
+    this.app_name = settings.app_name
+    this.host = settings.amq_host
+    if ('amq_virtualhost' in settings) {
+      this.virtualhost = settings.amq_virtualhost
+    } else {
+      this.virtualhost = consts.VIRTUALHOST
+    }
+    if ('amq_user' in settings) {
+      this.user = settings.amq_user
+    } else {
+      this.user = null
+    }
+    if ('amq_password' in settings) {
+      this.password = settings.amq_password
+    } else {
+      this.password = null
+    }
+    if ('amq_transport' in settings) {
+      this.transport = settings.amq_transport
+    } else {
+      this.transport = null
+    }
+    if ('amq_ttl' in settings) {
+      this.ttl = settings.amq_ttl
+    } else {
+      this.ttl = consts.TTL
+    }
+    if ('amq_msg_ttl' in settings) {
+      this.msg_ttl = settings.amq_msg_ttl
+    } else {
+      this.msg_ttl = consts.MSG_TTL
+    }
+    if ('amq_num_queues' in settings) {
+      this.num_queues = settings.amq_num_queues
+    } else {
+      this.num_queues = consts.NUM_QUEUES
+    }
+    if ('msg_encoding' in settings) {
+      this.msg_encoding = settings.msg_encoding
+    } else {
+      this.msg_encoding = consts.ENCODING
+    }
+    this.key = siphash.string16_to_key(consts.HASH)
   }
 
   get_hash(string) {
--- tests/unit.js
+++ tests/unit.js
@@ -12,14 +12,43 @@ describe('Array', function() {
 describe('Basics', function() {
   describe('Create Server', function () {
     it('the server should have a get_request and a put_result method', function () {
+      // TODO
       var server = new syrpc.SyRPCServer({})
       assert.equal(typeof server.get_hash, "function")
     })
+    it('pass the settings to instance', function () {
+      // TODO
+      var server = new syrpc.SyRPCServer({
+        app_name        : "1",
+        amq_host        : "2",
+        amq_virtualhost : "3",
+        amq_user        : "4",
+        amq_password    : "5",
+        amq_transport   : "6",
+        amq_ttl         : 7,
+        amq_msg_ttl     : 8,
+        amq_num_queues  : 9,
+        msg_encoding    : "10"
+      })
+      assert.equal(server.app_name, "1")
+      assert.equal(server.host, "2")
+      assert.equal(server.virtualhost, "3")
+      assert.equal(server.user, "4")
+      assert.equal(server.password, "5")
+      assert.equal(server.transport, "6")
+      assert.equal(server.ttl, 7)
+      assert.equal(server.msg_ttl, 8)
+      assert.equal(server.num_queues, 9)
+      assert.equal(server.msg_encoding, "10")
+    })
   })
   describe('Check hash function', function () {
-    it('the server should have a get_request and a put_result method', function () {
+    it('the results should match those of the python and php implementation', function () {
       var server = new syrpc.SyRPCServer({})
-      assert.equal(server.get_hash("huhu"), "Hello World")
+      assert.equal(server.get_hash("huhu"), 34)
+      assert.equal(server.get_hash("fasel"), 25)
+      assert.equal(server.get_hash("huh\xc3\xbc"), 53)
+      assert.equal(server.get_hash("THX\xe2\x84\xa2"), 54)
     })
   })
 })
