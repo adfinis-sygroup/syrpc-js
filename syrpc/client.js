d9a34dd1139cabb5055ddc6f045fe5c1e7cb5984
Damian Senn <damian.senn@adfinis-sygroup.ch>
2016-01-04 15:06:04 +0100

    Reject promise if getResponse() got an error

--- syrpc/client.js
+++ syrpc/client.js
@@ -62,6 +62,11 @@ export default class SyRPCClient extends SyRPCBase {
         this.channel.consume(result_queue, msg => {
           let res = JSON.parse(msg.content)
           debug(`Client: Got result ${res.result_id} on ${result_queue} `)
+          if (res.exception) {
+            reject(res.exception)
+
+            return
+          }
           if (res.result_id == result_id) {
             debug(`Client: Verified result on ${result_queue}`)
             resolve(res)
